e550d307bc6238657c1a52390a207c6ffe236829
FREDERICO SILVA DE ARAUJO <frederico.araujo@ibm.com>
2019-03-07 13:58:10 -0500

    exported file formatting

--- src/exporter.py
+++ src/exporter.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 #-------------------------------------------------------------------------------
 #
-# Watches for new monitoring files and uploads them to minio.io
+# Watches for new monitoring files and uploads them to cloud object store
 #
 # Instructions:
-#      ./monitor -h for help and command line options
+#      ./exporter -h for help and command line options
 #
 # F. Araujo (frederico.araujo@ibm.com)
 #
 #-------------------------------------------------------------------------------
 #
-import logging, argparse, codecs, sys, os, json, time
+import logging, argparse, codecs, sys, os, json, time, socket
 from executor import PeriodicExecutor
 from minio import Minio
 from minio.error import (ResponseError, BucketAlreadyOwnedByYou, BucketAlreadyExists)
@@ -18,18 +18,22 @@ from urllib3 import Timeout
 from urllib3.exceptions import MaxRetryError
 
 def files(path):  
+    """list files in dir path"""
     for file in os.listdir(path):
         if os.path.isfile(os.path.join(path, file)):
             yield os.path.join(path, file)
 
 def get_secret(secret_name):
+    """extract secret from vault, if using a vault"""    
     try:
-        with open('/run/secrets/%s' % (secret_name), 'r') as secret_file:
+        secrets_dir = '/run/secrets' if not (os.path.isdir('/run/secrets/k8s')) else '/run/secrets/k8s'
+        with open('%s/%s' % (secrets_dir, secret_name), 'r') as secret_file:
             return secret_file.read()
     except IOError:
         logging.exception('Caught exception while reading secret \'%s\'', secret_name)
 
 def cleanup(args):
+    """cleaup exported traces from local tmpfs""" 
     now = time.time()
     cutoff = now - (args.agemin * 60)
 
@@ -45,23 +49,22 @@ def cleanup(args):
                 os.remove(f)
 
 def run(args):
-
+    """main thread"""
     # Retrieve minio access and secret keys
-    access_key = get_secret('minio_access_key') if not args.accesskey else args.accesskey
-    secret_key = get_secret('minio_secret_key') if not args.secretkey else args.secretkey
+    access_key = get_secret('cos_access_key') if not args.accesskey else args.accesskey
+    secret_key = get_secret('cos_secret_key') if not args.secretkey else args.secretkey
 
     # Initialize minioClient with an endpoint and access/secret keys.
     minioClient = Minio('%s:%s' % (args.host, args.port),
 			access_key=access_key,
 			secret_key=secret_key,
-			secure=False)
+			secure=True)
     minioClient._http.connection_pool_kw['timeout'] = Timeout(connect=args.timeout, read=3*args.timeout)
 
     # Make a bucket with the make_bucket API call.
     try:
     	if not minioClient.bucket_exists(args.bucket):
             minioClient.make_bucket(args.bucket, location=args.location)        
-    
     except MaxRetryError:
         logging.error('Connection timeout! Removing traces older than %d minutes', args.agemin)
 	cleanup(args)
@@ -78,13 +81,15 @@ def run(args):
             traces = [f for f in files(args.dir)]
             traces.sort(key=lambda f: int(filter(str.isdigit, f)))
             # Upload complete traces, exclude most recent log
-            for trace in traces[:-1]:             
-                minioClient.fput_object(args.bucket, os.path.basename(trace), trace, metadata={'X-Amz-Meta-Trace': 'complete'})
+            for trace in traces[:-1]:
+                hostname = socket.gethostname()
+                ip = socket.gethostbyname(hostname)
+                minioClient.fput_object(args.bucket, os.path.basename(trace), '%s.%s' % (ip, trace), metadata={'x-amz-meta-host': hostname})
                 os.remove(trace)
                 logging.info('Uploaded trace %s', trace)
             # Upload partial trace without removing it
-	    minioClient.fput_object(args.bucket, os.path.basename(traces[-1]), traces[-1], metadata={'X-Amz-Meta-Trace': 'partial'})
-	    logging.info('Uploaded trace %s', traces[-1])
+	    #minioClient.fput_object(args.bucket, os.path.basename(traces[-1]), traces[-1], metadata={'X-Amz-Meta-Trace': 'partial'})
+	    #logging.info('Uploaded trace %s', traces[-1])
         except ResponseError:
             logging.exception('Caught exception while uploading traces to object store')
 
@@ -92,18 +97,18 @@ if __name__ == '__main__':
     
     # set command line args
     parser = argparse.ArgumentParser(
-        description='Monitor: client for watching and uploading monitoring files to minio.'
+        description='sf-exporter: service for watching and uploading monitoring files to object store.'
     )
-    parser.add_argument("--host", help='minio server address', default='localhost') 
-    parser.add_argument("--port", help='minio server port', default=9000)
-    parser.add_argument('--accesskey', help='minio access key', default=None)
-    parser.add_argument('--secretkey', help='minio secret key', default=None)
+    parser.add_argument('--host', help='cos server address', default='localhost') 
+    parser.add_argument('--port', help='cos server port', default=9000)
+    parser.add_argument('--accesskey', help='cos access key', default=None)
+    parser.add_argument('--secretkey', help='cos secret key', default=None)
     parser.add_argument('--scaninterval', help='interval between scans', type=float, default=1)
     parser.add_argument('--timeout', help='connection timeout', type=float, default=5)
     parser.add_argument('--agemin', help='number of minutes of traces to preserve in case of repeated timeouts', type=float, default=60)
     parser.add_argument('--dir', help='data directory', default='/mnt/data')
-    parser.add_argument('--bucket', help='target data bucket', default='monitoringdata')
-    parser.add_argument('--location', help='target data bucket location', default='appliance_monitoring_data')
+    parser.add_argument('--bucket', help='target data bucket', default='sf-monitoring')
+    parser.add_argument('--location', help='target data bucket location', default='us-south')
    
     # parse args and configuration
     args = parser.parse_args()
@@ -114,9 +119,9 @@ if __name__ == '__main__':
 
     try:
         logging.info('Running monitor task with host: %s:%s, bucket: %s, scaninterval: %ss', args.host, args.port, args.bucket, args.scaninterval)
-        monitor = PeriodicExecutor(args.scaninterval, run, [args])
-	monitor.run()
+        exporter = PeriodicExecutor(args.scaninterval, run, [args])
+	exporter.run()
     except:
-        logging.exception('Something bad happened')
+        logging.exception('Error while executing exporter')
 
     sys.exit(0)
