5a1b8fe65c443e28ce893a73b344d3fe8f0fe0aa
York Lin <yorklin@tw.ibm.com>
2020-04-13 18:18:59 +0800

    refine scripting bug
    Signed-off-by: York Lin <yorklin@tw.ibm.com>

--- build_scripts/redhat_scan_submit.sh
+++ build_scripts/redhat_scan_submit.sh
@@ -3,14 +3,11 @@
 # Usage: redhat_scan_submit.sh [login secret] [image_to_be_scanned] [RedHat project ID] [container label]
 set -e
 
-docker build --build-arg VERSION=test --build-arg RELEASE=$(git rev-parse --short HEAD) -t sf-exporter-runtime .
 docker login -u unused -p $1 scan.connect.redhat.com
 
-docker tag $(docker images sf-exporter-runtime -q) scan.connect.redhat.com/ospid-0597582f-0fe8-4fdc-b141-d20cc8a67f41/test:$1
-docker push scan.connect.redhat.com/ospid-0597582f-0fe8-4fdc-b141-d20cc8a67f41/test:$1
+docker tag $2 scan.connect.redhat.com/$3/test:$4
+docker push scan.connect.redhat.com/$3/test:$4
 
-docker rmi scan.connect.redhat.com/ospid-0597582f-0fe8-4fdc-b141-d20cc8a67f41/test:$1
-docker rmi sf-exporter-runtime
-docker image prune -f
+docker rmi scan.connect.redhat.com/$3/test:$4
 
 
